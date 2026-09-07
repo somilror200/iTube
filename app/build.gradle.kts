14e1259462f94b81c89717bf68923110da1c0cfe
Somil Garak <77724072+somilror200@users.noreply.github.com>
2026-09-07 12:36:56 +1000

    Wire RecyclerView into Android app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,12 +32,13 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation(libs.recyclerview)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
